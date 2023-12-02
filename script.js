0371a38ce7ada8ab9721a7f716bb5b111eac36d5
hashpfonseka <prabhodahashini55@gmail.com>
2023-12-03 02:53:56 +0530

    color feature added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function applyFormat(command) {
     document.execCommand(command, false, null);
 }
+
+document.getElementById('fontColorPicker').addEventListener('input', function() {
+    var color = this.value;
+    document.execCommand('foreColor', false, color);
+});
